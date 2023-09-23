4fdde7cefd98387ce9075ef4b5e5b9312d41be7a
UnplugCharger <isaacbyron@gmail.com>
2023-09-23 18:04:56 +0300

    wip

--- db/migrations/000002_advice.up.sql
+++ db/migrations/000002_advice.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE receipts_advice (
-                                 advice_id SERIAL PRIMARY KEY,
-                                 processed_id INT REFERENCES receipts_processed(processed_id) ON DELETE CASCADE,
-                                 advice TEXT NOT NULL,
-                                 date_generated DATE DEFAULT CURRENT_DATE,
-                                 metadata JSONB
+advice_id SERIAL PRIMARY KEY,
+processed_id INT REFERENCES receipts_processed(processed_id) ON DELETE CASCADE,
+advice TEXT NOT NULL,
+date_generated DATE DEFAULT CURRENT_DATE,
+metadata JSONB
 );
